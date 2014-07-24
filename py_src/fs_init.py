f08598d14d151e43d06a7cbe996e15dec65e8e64
Dave van Soest <dave.soest@gfk.com>
2014-07-25 00:04:04 +0200

    Added and modified Redis LUA scripts.

--- py_src/fs_init.py
+++ py_src/fs_init.py
@@ -7,7 +7,7 @@ from connect import client as rc
 def reset():
 	rc.flushdb()
 	rootDirId = rc.incr('curNodeId')
-	rc.hmset('node:{}'.format(rootDirId), {'type': 'dir', 'ref': 1})
+	rc.hmset('node:{}'.format(rootDirId), {'type': 'dir', 'ref': 1, 'mode': 0})
 	rc.hmset('fsdata', {'rootDirId': rootDirId})
 	load_scripts()
 
@@ -37,7 +37,7 @@ def load_scripts():
 	script_sha['dir_create'] = rc.script_load(
 		helpers['findNodeId'] + """
 		local dirid = redis.call('incr', 'curNodeId')
-		redis.call('hmset', 'node:' .. dirid, 'type', 'dir', 'ref', 1)
+		redis.call('hmset', 'node:' .. dirid, 'type', 'dir', 'ref', 1, 'mode', 0)
 		local parentid = findNodeId(ARGV[1])
 		if parentid == false then return false end
 		if redis.call('hget', 'node:' .. parentid, 'type') ~= 'dir' then return false end
@@ -56,16 +56,41 @@ def load_scripts():
 	""")
 
 
+	## Retrieve attributes:
+	script_sha['getattr'] = rc.script_load(
+		helpers['findNodeId'] + """
+		local nodeid = findNodeId(ARGV[1])
+		if nodeid == false then return false end
+		local info = redis.call('hmget', 'node:' .. nodeid, 'type', 'mode')
+		if info[1] == 'file' then
+			info[3] = '' .. redis.call('strlen', 'node:' .. nodeid .. ':data')
+		else
+			info[3] = '0'
+		end
+		return info
+	""")
+
+
 	## Create file:
 	script_sha['file_create'] = rc.script_load(
 		helpers['findNodeId'] + """
 		local fileid = redis.call('incr', 'curNodeId')
-		redis.call('hmset', 'node:' .. fileid, 'type', 'file', 'ref', 1)
+		redis.call('hmset', 'node:' .. fileid, 'type', 'file', 'ref', 1, 'mode', 0)
 		redis.call('set', 'node:' .. fileid .. ':data', ARGV[3])
 		local parentid = findNodeId(ARGV[1])
 		if parentid == false then return false end
 		if redis.call('hget', 'node:' .. parentid, 'type') ~= 'dir' then return false end
 		redis.call('hset', 'node:' .. parentid .. ':entries', ARGV[2], fileid)
+		return fileid
+	""")
+
+
+	## Open file:
+	script_sha['file_open'] = rc.script_load(
+		helpers['findNodeId'] + """
+		local fileid = findNodeId(ARGV[1])
+		if fileid == false then return -1234 end
+		if redis.call('hget', 'node:' .. fileid, 'type') ~= 'file' then return -2345 end
 		return fileid
 	""")
 
@@ -77,6 +102,21 @@ def load_scripts():
 		if fileid == false then return false end
 		if redis.call('hget', 'node:' .. fileid, 'type') ~= 'file' then return false end
 		return redis.call('get', 'node:' .. fileid ..':data')
+	""")
+
+
+	## Read file chunk by file id:
+	script_sha['fileid_read_chunk'] = rc.script_load("""
+		local fileid = ARGV[1]
+		if fileid == false then return false end
+		if redis.call('hget', 'node:' .. fileid, 'type') ~= 'file' then return false end
+		if ARGV[2] ~= nil and ARGV[3] ~= nil then
+			local offset = ARGV[2]
+			local maxsize = ARGV[3]
+			return redis.call('getrange', 'node:' .. fileid .. ':data', offset, offset + maxsize - 1)
+		else
+			return redis.call('get', 'node:' .. fileid ..':data')
+		end
 	""")
 
 
@@ -102,4 +142,6 @@ def load_scripts():
 
 	# Write script ids:
 	rc.hmset('scripts', script_sha)
+
+	print(script_sha)
 
